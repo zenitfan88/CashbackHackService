68e73968daf084d11623881bd0fb5d0deb3f2512
zenitfan88 <nikanenkov@rambler.ru>
2022-10-17 21:30:13 +0400

    fixed test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -4,9 +4,30 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+
 public class CashbackHackServiceTest {
 
+    CashbackHackService service = new CashbackHackService();
+
     @Test
-    public void
+    public void buyingLessThousand() {
+        int actual = service.remain(879);
+        int expected = 121;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void buyingOverThousand() {
+        int actual = service.remain(2375);
+        int expected = 625;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void buyingThousand() {
+        int actual = service.remain(1000);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
 
 }

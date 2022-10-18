cad0c7228dc86cbc5a0cbf525f53c0eace2a28a9
zenitfan88 <nikanenkov@rambler.ru>
2022-10-18 15:00:49 +0400

    added junit4-platform

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 
 public class CashbackHackServiceTest {
@@ -30,4 +30,24 @@ public class CashbackHackServiceTest {
         assertEquals(actual, expected);
     }
 
+    @org.junit.jupiter.api.Test
+    public void buyingLessThousandJunitJupiter() {
+        int actual = service.remain(879);
+        int expected = 121;
+        assertEquals(actual, expected);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void buyingOverThousandJunitJupiter() {
+        int actual = service.remain(2375);
+        int expected = 625;
+        assertEquals(actual, expected);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void buyingThousandJunitJupiter() {
+        int actual = service.remain(1000);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
 }
